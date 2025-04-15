44d340052d931b3bbfe42c0beda648394ca79f91
Richard Ludwig <vladtepesch@Mac.fritz.box>
2025-04-15 15:25:31 +0200

    PEP8 Update

--- filter_users.py
+++ filter_users.py
@@ -4,9 +4,7 @@ import json
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
-
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
-
     for user in filtered_users:
         print(user)
 
@@ -14,7 +12,6 @@ def filter_users_by_name(name):
 def filter_user_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
-
     filtered_users = [user for user in users if user["age"] == age]
     for user in filtered_users:
         print(user)
@@ -23,7 +20,6 @@ def filter_user_by_age(age):
 def filter_user_by_email(email):
     with open("users.json", "r") as file:
         users = json.load(file)
-
     filtered_users = [user for user in users if user["email"] == email]
     for user in filtered_users:
         print(user)
@@ -31,7 +27,6 @@ def filter_user_by_email(email):
 
 if __name__ == "__main__":
     filter_option = input("What would you like to filter by? (Currently, 'name', 'age' and 'email' is supported): ").strip().lower()
-
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
